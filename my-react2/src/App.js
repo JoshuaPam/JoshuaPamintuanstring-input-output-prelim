09cfc67ad362eb11ea6045cc19c40b545e538fbf
JoshuaPam <Joshua_Pamintuan@rcc.edu.ph>
2024-09-28 11:22:45 +0800

    my-react-app2

---
+++ my-react2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import StringOutput from './StringOutput';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StringOutput />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react2/src/StringOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StringOutput() {
+  const message = "Hello, this is a string output!";
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default StringOutput;
